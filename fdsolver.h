d86f1a566b7b9c5cc051c19d5e69fb1629f36739
pqwang1026 <pqwang1026@gmail.com>
2016-03-06 15:54:22 -0500

    Created header files.

---
+++ fdsolver.h
@@ -0,0 +1,18 @@
+//  Created by peiqiw on 06/03/2016.
+//  Copyright (c) 2016 peiqiw. All rights reserved.
+//
+
+#ifndef fd_solver_h
+#define fd_solver_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+using namespace std;
+
+class FiniteDiffSolver {
+    
+private:
+
+}
---
+++ model.h
@@ -0,0 +1,18 @@
+//  Created by peiqiw on 06/03/2016.
+//  Copyright (c) 2016 peiqiw. All rights reserved.
+//
+
+#ifndef cybermfg_model_h
+#define cybermfg_model_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+using namespace std;
+
+class Model {
+    
+private:
+
+}
